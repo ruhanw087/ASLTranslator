1e440bc5dde73be4251aa05a0e05a8e4677eb304
Ruhan Waghmare <rug@macbook-pro.lan>
2025-11-30 18:32:45 -0800

    adding socket io to ec2 server

--- src/app.py
+++ src/app.py
@@ -1,50 +1,37 @@
-from flask import Flask, Response, jsonify, request, send_file
-from functional.real_time_prediction import predict_frame, initialization, prediction_generator
-import numpy as np
-import mediapipe as mp
-import cv2
-import io
+# server.py
+from flask import Flask
 from flask_cors import CORS
-
+from flask_socketio import SocketIO, emit
+import cv2
+import numpy as np
+import base64
+import cv2
+from functional.real_time_prediction import predict_frame, initialization
 
 app = Flask(__name__)
 CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+model = None
+hands = None
+mp_hands = None
+mp_drawing = None
+
 initialization('RandomForest')
 
-@app.route("/video_prediction")
-def video_prediction():
-    return Response(
-        prediction_generator(),
-        mimetype='multipart/x-mixed-replace; boundary=frame'
-    )
-
-@app.route("/predict_frame", methods=["POST"])
-def predict_frame_api():
-    if 'image' not in request.files:
-        return {"error": "No image uploaded"}, 400
-
-    file = request.files['image']
-    file_bytes = np.frombuffer(file.read(), np.uint8)
-    frame = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-
-    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    val = predict_frame(frame, image)
-
-    cv2.putText(frame, str(val), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2, cv2.LINE_AA)
-
-    ret, buffer = cv2.imencode('.jpg', frame)
-    if not ret:
-        return {"error": "Failed to encode frame"}, 500
-
-    return send_file(
-        io.BytesIO(buffer.tobytes()),
-        mimetype='image/jpeg'
-    )
-
-
-@app.route("/status")
-def status():
-    return jsonify({"status": "API is running"})
+@socketio.on('frame')
+def handle_frame(data):
+    b64data = data.split(',')[1]
+    img_bytes = base64.b64decode(b64data)
+    np_arr = np.frombuffer(img_bytes, np.uint8)
+    frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+    frame = cv2.flip(frame,1)
+    image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    prediction = predict_frame(frame, image_rgb)
+    cv2.putText(frame, str(prediction), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2, cv2.LINE_AA)
+    _, buffer = cv2.imencode('.jpg', frame)
+    jpg_as_text = base64.b64encode(buffer).decode('utf-8')
+    emit('processed_frame', f"data:image/jpeg;base64,{jpg_as_text}")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    socketio.run(app, host='0.0.0.0', port = 5000)
